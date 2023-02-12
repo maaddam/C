c25345fec88101809863cc53e24d2a97624d85ca
maaddam <124426926+maaddam@users.noreply.github.com>
2023-02-13 00:02:09 +0900

    Add files via upload

---
+++ 3단계/3단계 5번.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int T, A, B;
+	scanf("%d", &T);
+	
+	int i;
+	for (i = 0; i < T; i++)
+	{
+		scanf("%d %d", &A, &B);
+		printf("%d", A + B);
+	}
+
+	return 0;
+}
